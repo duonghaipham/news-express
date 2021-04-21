fb8ad126c6fff9ef76df9dbbff74dc77ab8f809d
= <phduongit2k1@gmail.com>
2021-04-21 16:39:09 +0700

    Prevent accessing from data folder, hide 'index.php' in address bar, change 'control' folder into 'controller'

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-require 'control/BaseController.php';
+require 'controller/BaseController.php';
 require 'model/core/Database.php';
 //$db = new Database;
 
 $controller_name = ucfirst(($_REQUEST['controller'] ?? 'Layout') . 'Controller');
 $action_name = strtolower($_REQUEST['action'] ?? 'index');
 
-require "control/${controller_name}.php";
+require "controller/${controller_name}.php";
 
 $controller_object = new $controller_name;
 $controller_object->$action_name();
